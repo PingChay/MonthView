a816b9821d32e8879b5481b651de7cf3bcc61b45
Saran Nonkamjan <saran.non@gmail.com>
2014-09-04 11:42:53 +0700

    Initial Commit

---
+++ CalendarCollectionView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CalendarCollectionView
+//
+//  Created by PingChay 's MacMiNi on 9/4/14.
+//  Copyright (c) 2014 Saran Nonkamjan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CalendarCollectionView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CalendarCollectionView
+//
+//  Created by PingChay 's MacMiNi on 9/4/14.
+//  Copyright (c) 2014 Saran Nonkamjan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
